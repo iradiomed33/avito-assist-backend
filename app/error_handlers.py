ee39ec5b449cad6b3f174f21d386d3c803c353fc
iradiomed33 <iradiomed33@gmail.com>
2025-11-27 17:53:45 +0300

    fix asic uth -uthenticate header

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -2,7 +2,7 @@
 Централизованная обработка ошибок и исключений FastAPI.
 """
 
-from fastapi import Request, status
+from fastapi import Request, status, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
